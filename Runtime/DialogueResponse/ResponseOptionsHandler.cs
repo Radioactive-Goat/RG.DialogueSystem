33b0dd7cdb9461757f4884d861baf626ddbe022c
BlankRip <aniketh_g5@outlook.com>
2024-01-14 13:30:58 +0530

    Skeliton code almost done

--- Runtime/DialogueResponse/ResponseOptionsHandler.cs
+++ Runtime/DialogueResponse/ResponseOptionsHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,38 +11,99 @@ namespace RG.DialogueSystem
         [SerializeField] GameObject _responsesUiObject;
         [SerializeField] UiResponseOption[] _uiResponseOptions;
         [SerializeField] bool _shouldLoopNavigation;
+        public Action OnEndConversationResponse;
 
-        private DialogResponseCollection activeResponseCollection;
+        private DialogResponseCollection _activeResponseCollection;
         private int _maxPossibleResponses;
         private int _currentHighlightedIndex;
 
         private void Start()
         {
             _maxPossibleResponses = _uiResponseOptions.Length;
-            _currentHighlightedIndex = 0;
+            _currentHighlightedIndex = -1;
         }
 
-        public void ShowResponsOptions(DialogResponseCollection responses)
+        public void ShowResponseOptions(DialogResponseCollection responseCollection)
         {
+            if(responseCollection.Responses.Length > _uiResponseOptions.Length)
+            {
+                Debug.LogError($"The responses in the collection; {responseCollection.name}, are greater than available UI slots");
+                return;
+            }
 
+            ResetResponsesUI();
+            _activeResponseCollection = responseCollection;
+            CharactorDisplayHandler.Instance.DisplayCharactor(_activeResponseCollection.RespondingCharactor);
+            for (int i = 0; i < responseCollection.Responses.Length; i++)
+            {
+                _uiResponseOptions[i].SetResponseText(responseCollection.Responses[i].Response);
+            }
+            HighlightResponseOption(0);
+            _responsesUiObject.SetActive(true);
         }
 
         public void SelectResponse()
         {
-
+            _uiResponseOptions[_currentHighlightedIndex].ConfirmOption();
+            _responsesUiObject.SetActive(false);
+            if (_activeResponseCollection.Responses[_currentHighlightedIndex].FollowUpDialogues != null)
+            {
+                DialogFlowHandler.Instance.StartNewDialogChain(_activeResponseCollection.Responses[_currentHighlightedIndex].FollowUpDialogues);
+            }
+            else
+            {
+                OnEndConversationResponse?.Invoke();
+            }
         }
 
         public void NavigateUp()
         {
-
+            int newHighlight = _currentHighlightedIndex - 1;
+            if(newHighlight < 0)
+            {
+                if(_shouldLoopNavigation)
+                {
+                    newHighlight = _activeResponseCollection.Responses.Length - 1;
+                }
+                else
+                {
+                    newHighlight = 0;
+                }
+            }
+            HighlightResponseOption(newHighlight);
         }
 
         public void NavigateDown()
         {
-
+            int newHighlight = _currentHighlightedIndex + 1;
+            if (newHighlight >= _activeResponseCollection.Responses.Length)
+            {
+                if (_shouldLoopNavigation)
+                {
+                    newHighlight = 0;
+                }
+                else
+                {
+                    newHighlight = _currentHighlightedIndex;
+                }
+            }
+            HighlightResponseOption(newHighlight);
         }
 
-        public void ResetResponcesUI()
+        private void HighlightResponseOption(int index)
+        {
+            if(_currentHighlightedIndex != index)
+            {
+                if(_currentHighlightedIndex >= 0)
+                {
+                    _uiResponseOptions[_currentHighlightedIndex].RemoveHighlight();
+                }
+                _currentHighlightedIndex = index;
+                _uiResponseOptions[_currentHighlightedIndex].HighlightOption();
+            }
+        }
+
+        public void ResetResponsesUI()
         {
             foreach (UiResponseOption responseOption in _uiResponseOptions)
             {
